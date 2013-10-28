43d1450cf8158c1e794a831c4999fcc6e02ae4b6
Neil Turner <neilturner@gmail.com>
2013-10-28 18:15:19 +0000

    Added map control and animation code

--- SlidingPivotDemo/SlidingPivotDemo/MainPage.xaml.cs
+++ SlidingPivotDemo/SlidingPivotDemo/MainPage.xaml.cs
@@ -10,12 +10,14 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using System.ComponentModel;
 
 namespace SlidingPivotDemo
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        // Constructor
+        private bool isMapOpen;
+
         public MainPage()
         {
             InitializeComponent();
@@ -23,6 +25,8 @@ namespace SlidingPivotDemo
             // Set the data context of the listbox control to the sample data
             DataContext = App.ViewModel;
             this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+
+            this.isMapOpen = false;
         }
 
         // Load data for the ViewModel Items
@@ -31,6 +35,60 @@ namespace SlidingPivotDemo
             if (!App.ViewModel.IsDataLoaded)
             {
                 App.ViewModel.LoadData();
+            }
+        }
+
+        private void PivotItem_DoubleTap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            resizePivot();
+        }
+
+        private void resizePivot()
+        {
+            double up = 0;
+            double down = 478;
+
+            double from, to;
+            var easeAnimation = new CubicEase();
+
+            if (!this.isMapOpen)
+            {
+                from = up;
+                to = down;
+                easeAnimation.EasingMode = EasingMode.EaseOut;
+                this.isMapOpen = true;
+            }
+            else
+            {
+                from = down;
+                to = up;
+                easeAnimation.EasingMode = EasingMode.EaseOut;
+                this.isMapOpen = false;
+            }
+
+            var translateTransform = new TranslateTransform();
+            MainPivot.RenderTransform = translateTransform;
+
+            Storyboard sb = new Storyboard();
+            DoubleAnimation moveAnimation = new DoubleAnimation();
+            moveAnimation.EasingFunction = easeAnimation;
+            moveAnimation.From = from;
+            moveAnimation.To = to;
+            moveAnimation.Duration = new Duration(TimeSpan.FromSeconds(0.5));
+
+            Storyboard.SetTarget(moveAnimation, translateTransform);
+            Storyboard.SetTargetProperty(moveAnimation, new PropertyPath(TranslateTransform.YProperty));
+
+            sb.Children.Add(moveAnimation);
+            sb.Begin();
+        }
+
+        protected override void OnBackKeyPress(CancelEventArgs e)
+        {
+            if (isMapOpen)
+            {
+                resizePivot();
+                e.Cancel = true;
             }
         }
     }
